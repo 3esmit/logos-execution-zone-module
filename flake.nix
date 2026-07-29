e8d84103660604b1a6a06ddd66d20da7a2fdeb3f
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2026-07-28 22:02:35 -0300

    fix(deps): include Testnet transaction lookup fix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
     nix-bundle-lgx.url = "github:logos-co/nix-bundle-lgx";
-    logos-execution-zone.url = "github:3esmit/logos-execution-zone?rev=6e0013f5b771fc1b96a65361b531f289111b8aaa";
+    logos-execution-zone.url = "github:3esmit/logos-execution-zone?rev=e923315c020d4966807849f9db10536b628d5739";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
