d7fd14afdef34df2b34baa5a3bb0f5fcefa95bb9
3esmit <3esmit@gmail.com>
2026-08-08 15:53:06 -0300

    fix(deps): pin module builder to maintained fork

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Logos Execution Zone Core Module";
 
   inputs = {
-    logos-module-builder.url = "github:logos-co/logos-module-builder";
+    logos-module-builder.url = "github:3esmit/logos-module-builder?rev=6ef42ea8661121831ece79e6b702e27ac1cf46e7";
     nix-bundle-lgx.url = "github:logos-co/nix-bundle-lgx";
     logos-execution-zone.url = "github:3esmit/logos-execution-zone?rev=685d07dee11a9d934fa9a205de73f356dfbeb6d7";
   };
